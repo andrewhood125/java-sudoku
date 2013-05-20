41119943fc84fc920f261d0cda22c29a6a3537a9
Andrew Hood <andrewhood125@gmail.com>
2013-05-20 18:39:38 -0500

    Adding interactive mode selection.
    1 for single player. 2. for solver.

--- Sudoku.java
+++ Sudoku.java
@@ -1,3 +1,6 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 
 /**
  * Write a description of class Sudoku here.
@@ -11,7 +14,41 @@ public class Sudoku
     {
         Gameboard board = new Gameboard();
         board.loadSudoku();
-        Player player = new NormalPlayer(board);
+        
+        System.out.println("#################################");
+        System.out.println("# Sudoku   (c) 2013 Andrew Hood #");
+        System.out.println("# 1) Single Player. 2) Solver   #");
+        System.out.println("#################################");
+        int whichType = 0;
+        Scanner input = new Scanner(System.in);
+        
+        boolean badInput = false;
+        do
+        {
+        	try 
+        	{
+        		System.out.print("~$");
+        		whichType = input.nextInt();
+        		badInput = false;
+        	} catch (InputMismatchException ex) {
+        		System.err.println("1 for Single Player, 2 for Solver.");
+        		badInput = true;
+        		input.nextLine();
+        	}
+        	
+        } while(badInput || whichType < 1 || whichType > 2);
+        
+        Player player = null;
+        if(whichType == 1)
+        {
+        	 player = new NormalPlayer(board);
+        } else if(whichType == 2) {
+        	player = new ComputerPlayer(board);
+        } else {
+        	System.exit(1);
+        }
+        
+        
         while(!board.isComplete())
         {
             Move tempMove = player.nextMove();
